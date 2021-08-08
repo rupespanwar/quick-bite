845c65cf7984da818148cfa4cdea6c9c2cac8e89
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-08-08 16:45:12 +0530

    basic express route setup

---
+++ MERN/Full-Stack-App/server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hi there')
+})
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => `Server running on port ${port} 🔥`);
